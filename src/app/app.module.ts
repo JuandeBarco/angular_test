ddeb9900bc54944091567f34d36e3ef138cdc07a
JuandeBarco <34903133+JuandeBarco@users.noreply.github.com>
2022-02-18 15:44:21 -0600

    Primera semana

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-import { PersonaComponent } from './persona/persona.component';
-import { ListadoComponent } from './listado/listado.component';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ContadorComponent,
-    PersonaComponent,
-    ListadoComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
